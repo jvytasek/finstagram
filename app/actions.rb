fa3e118c9e62080f587fdd91b270a7d001bba33c
Jovita <jvytasek@gmail.com>
2017-09-01 03:34:48 +0000

    adding a login page

--- app/actions.rb
+++ app/actions.rb
@@ -1,9 +1,33 @@
 
 
 get '/' do
-
    @posts = Post.order(created_at: :desc)
-
     erb(:index)
+end
 
+
+get'/signup' do #if a user navigates to the path "/signup"
+    @user = User.new #set up empty object
+    erb(:signup)
+end
+
+    
+
+post '/signup' do
+    
+    #grab user input values from params
+    email   =params[:email]
+    avatar_url  =params[:avatar_url]
+    username    =params[:username]
+    password    =params[:password]
+
+    
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    if @user.save
+        "User #{username} saved!"
+    
+    else
+        erb(:signup)
+    end
 end
